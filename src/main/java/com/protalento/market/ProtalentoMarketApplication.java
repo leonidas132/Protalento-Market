e0bd0ef17d37ad9b0d708b0d5387dce23bde264e
luis miguel <luismrocha132@gmail.com>
2022-09-11 14:26:51 -0500

    primer commit del proyecto

---
+++ src/main/java/com/protalento/market/ProtalentoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.protalento.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtalentoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtalentoMarketApplication.class, args);
+	}
+
+}
